567c9a4342906d0dc50f321dd4b4802295dfc38f
OleksandrPshenychnyi <73752533+OleksandrPshenychnyi@users.noreply.github.com>
2020-11-04 23:56:21 +0200

    firstcommit
    download

---
+++ Lab1/Lab1.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+#include <conio.h>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+ float F(float x) {
+    return 80 * x - 30 * pow(x, 2) - 0.25 * pow(x, 4);
+}
+int main()
+{
+    float a, b, x, x1, x2, f1, f2, eps, fm;
+    int n = 0;
+    cout<<("Vvedite tochnost' eps:");
+    cin>>eps;
+    cout<<("Vvedite a & b:");
+    cin>>a >> b;
+    while (fabs(b - a) > eps) {
+        cout << "a=" << a << "\t"<<"b="<<b<<"\t";
+        x = (a + b) / 2;
+        x1 = x - eps;
+        x2 = x + eps;
+        cout << "x1=" << x1 << "\t"<< "x2="<<x2<<"\t";
+        f1 = F(x1);
+        f2 = F(x2);
+        cout << "f1=" << f1 << "\t" << "f2=" << f2 << "\n";
+        if (1 * f1 > 1 * f2) {
+            b = x;
+        }
+        else { a = x; }
+        n++;
+    }
+    x = (b + a) / 2;
+    fm = F(x);
+    cout<<"Цільова функція="<<x<< "\t Fmax="<<fm<< "\t Kilkist iteraciy="<<n;
+    _getch();
+}
